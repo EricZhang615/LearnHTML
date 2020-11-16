324478f5ff2bed7b17864810b12fe1138e40e6ce
EricZhang615 <eric20000615@gmail.com>
2020-11-16 21:37:06 +0800

    背景轮换功能 - finished

--- web/SwitchBkgImg.js
+++ web/SwitchBkgImg.js
@@ -1,1 +1,35 @@
 
+var imgList=["bkgImg1","bkgImg2","bkgImg3","bkgImg4","bkgImg5","bkgImg6"];
+var index1=0;
+var index2=2;
+
+var timeFade = null;
+
+function fadeOut(){
+    var presentImg = document.getElementById(imgList[index1]);
+    timeFade = setInterval(function (){
+        presentImg.style.opacity -= 0.01;
+        if (presentImg.style.opacity == 0){
+            clearInterval(timeFade);
+            for (var i=0;i<6;i++){
+                document.getElementById(imgList[i]).style.zIndex++;
+                if (document.getElementById(imgList[i]).style.zIndex == 7){
+                    document.getElementById(imgList[i]).style.zIndex = 1;
+                }
+            }
+            index1++;
+            if (index1 == 6){
+                index1 = 0;
+            }
+            index2++;
+            if (index2 == 6){
+                index2 = 0;
+            }
+        }
+    },10);
+}
+
+setInterval(function (){
+    document.getElementById(imgList[index2]).style.opacity = 1;
+    fadeOut();
+},5000);
